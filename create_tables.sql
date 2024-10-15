de4610312e364a91ce96fbe08a5bff68a3a6548e
Brian Pham <bnp200000@utdallas.edu>
2024-10-15 06:32:11 -0500

    Refactored some of the tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,44 +1,41 @@
 CREATE TABLE
     IF NOT EXISTS "USER" (
-        user_id VARCHAR(25) NOT NULL,
+        user_id VARCHAR(25) PRIMARY KEY,
         username VARCHAR(25) NOT NULL,
         password VARCHAR(25) NOT NULL,
         email VARCHAR(25),
         account_type VARCHAR(25),
         has_free_chat BOOLEAN,
         has_safe_chat BOOLEAN,
-        has_safe_server_access BOOLEAN,
-        PRIMARY KEY (user_id)
+        has_safe_server_access BOOLEAN
     );
 
 CREATE TABLE
     IF NOT EXISTS "CLASS" (
-        class_id VARCHAR(25) NOT NULL,
+        class_id VARCHAR(25) PRIMARY KEY,
         class_name VARCHAR(25),
         class_description VARCHAR(25),
-        class_role VARCHAR(25),
-        PRIMARY KEY (class_id)
+        class_role VARCHAR(25)
     );
 
 CREATE TABLE
     IF NOT EXISTS "CHARACTER" (
-        character_id VARCHAR(25) NOT NULL,
-        exp_level INT,
-        character_name VARCHAR(25),
+        character_id VARCHAR(25) PRIMARY KEY,
+		exp_level INT,
+		character_name VARCHAR(25), 
         gold_balance INT,
         owner_id VARCHAR(25) NOT NULL,
         character_class VARCHAR(25),
-        party_leader VARCHAR(25),
-        PRIMARY KEY (character_id),
-        FOREIGN KEY (owner_id) REFERENCES "USER" (user_id), -- "OWNS" relationship, 1 to N
-        FOREIGN KEY (character_class) REFERENCES "CLASS" (class_id), -- "BELONGS TO" relationship, 1 to 1
-        FOREIGN KEY (party_leader) REFERENCES "CHARACTER" (character_id) -- "MANAGES" relationship, 1 to N
+        leader_id VARCHAR(25),
+        FOREIGN KEY (owner_id) REFERENCES "USER" (user_id) ON DELETE CASCADE ON UPDATE CASCADE, -- "OWNS" relationship, 1 to N
+        FOREIGN KEY (character_class) REFERENCES "CLASS" (class_id) ON DELETE CASCADE ON UPDATE CASCADE -- "BELONGS TO" relationship, 1 to 1
+        --FOREIGN KEY (leader_id) REFERENCES "CHARACTER" (character_id) "CHARACTER" (character_id) ON DELETE CASCADE ON UPDATE CASCADE, -- "MANAGES" relationship, 1 to N
     );
 
 CREATE TABLE
     IF NOT EXISTS "CHARACTER_FRIEND" (
-        character_a_id VARCHAR(25) NOT NULL,
-        character_b_id VARCHAR(25) NOT NULL,
+        character_a_id VARCHAR(25),
+        character_b_id VARCHAR(25),
         PRIMARY KEY (character_a_id, character_b_id),
         FOREIGN KEY (character_a_id) REFERENCES "CHARACTER" (character_id),
         FOREIGN KEY (character_b_id) REFERENCES "CHARACTER" (character_id)
@@ -47,7 +44,7 @@ CREATE TABLE
 CREATE TABLE
     IF NOT EXISTS "PARTY" (
         party_name VARCHAR(50) UNIQUE,
-        party_leader VARCHAR(25),
+        party_leader VARCHAR(25) UNIQUE,
         party_balance INT,
         PRIMARY KEY (party_name, party_leader),
         FOREIGN KEY (party_leader) REFERENCES "CHARACTER" (character_id) ON DELETE CASCADE ON UPDATE CASCADE
@@ -66,7 +63,7 @@ CREATE TABLE
 CREATE TABLE
     IF NOT EXISTS "IN_INVENTORY" (
         character_id VARCHAR(25) NOT NULL,
-        item_id int NOT NULL,
+        item_id SERIAL NOT NULL,
         quantity int,
         FOREIGN KEY (character_id) REFERENCES "CHARACTER" (character_id) ON DELETE CASCADE,
         FOREIGN KEY (item_id) REFERENCES "ITEM" (item_id) ON DELETE CASCADE
@@ -76,7 +73,7 @@ CREATE TABLE
     IF NOT EXISTS "LISTING" (
         listing_id SERIAL PRIMARY KEY,
         character_id VARCHAR(25) NOT NULL,
-        item_id int NOT NULL,
+        item_id SERIAL NOT NULL,
         quantity int NOT NULL,
         listing_date DATE NOT NULL,
         is_active BOOLEAN,
@@ -89,7 +86,11 @@ CREATE TABLE
     IF NOT EXISTS "TRANSACTION" (
         transaction_id CHAR(10) PRIMARY KEY,
         listing_id SERIAL,
+		seller_id VARCHAR(25) NOT NULL,
+		buyer_id VARCHAR(25) NOT NULL,
         total_price INT,
         transaction_date DATE,
-        FOREIGN KEY (listing_id) REFERENCES "LISTING" (listing_id)
+		FOREIGN KEY (seller_id) REFERENCES "CHARACTER" (character_id) ON DELETE CASCADE ON UPDATE CASCADE,
+		FOREIGN KEY (buyer_id) REFERENCES "CHARACTER" (character_id) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (listing_id) REFERENCES "LISTING" (listing_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
