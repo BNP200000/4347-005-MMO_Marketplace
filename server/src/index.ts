71f79c88182f24e881e96c6d49c402ae973912f9
Brian Pham <bnp200000@utdallas.edu>
2024-10-29 11:46:30 -0500

    feat: Added postgre to package

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,20 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const marketplace = require("./marketplace");
+
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Express!");
+app.get("/", (req: Request, res: Response) => { 
+  marketplace.getUser().then((response) => {
+    res.status(200).send(response);
+  })
+  .catch((error) => {
+    res.status(500).send(error);
+  })
+  //res.status(200).send("Hello from Express!");
 });
 
 app.listen(PORT, () => {
---
+++ server/src/marketplace.ts
@@ -0,0 +1,88 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'bpham02',
+    host: 'localhost',
+    database: 'MMO_Marketplace',
+    password: 'Platinum32$',
+    port: 5432
+});
+
+//  USER table
+
+// GET USER
+const getUser = async() => {
+    try {
+        return await new Promise(function (resolve, reject) {
+            pool.query("SELECT * FROM \"USER\"", (error, results) => {
+                if(error) {
+                    reject(error);
+                }
+                
+                if(results && results.rows) {
+                    resolve(results.rows);
+                } else {
+                    reject(new Error("No results found"));
+                }
+            });
+        });
+    } catch (error_1) {
+        console.error(error_1);
+        throw new Error("Internal server error");
+    }
+};
+
+// CREATE USER
+const createUser = (body) => {
+    return new Promise(function (resolve, reject) {
+        const {
+            user_id, username, email, 
+            password, account_type, 
+            has_free_chat, has_safe_chat, 
+            has_safe_server_access} = body;
+        pool.query(
+            "INSERT INTO \"USER\" \
+            (user_id, username, email, password, account_type, \
+            has_free_chat, has_safe_chat, has_safe_server_access) \
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+            [user_id, username, email, password, account_type, 
+                has_free_chat, has_safe_chat, 
+                has_safe_server_access],
+            (error, results) => {
+                if(error) {
+                    reject(error);
+                }
+                    
+                if(results && results.rows) {
+                    resolve(
+                        `A new user has been added: ${JSON.stringify(results.rows[0])}`
+                    )
+                } else {
+                    reject(new Error("No results found"));
+                }
+            }
+        );
+    });
+};
+
+const deleteUser = (user_id) => {
+    return new Promise(function (resolve, reject) {
+        pool.query(
+            "DELETE FROM \"USER\" WHERE user_id = $1",
+            [user_id],
+            (error, results) => {
+                if(error) {
+                    reject(error);
+                }
+
+                resolve(`User deleted with ID: ${user_id}`);
+            }
+        );
+    });
+};
+
+module.exports = {
+    getUser,
+    createUser,
+    deleteUser
+};
+
