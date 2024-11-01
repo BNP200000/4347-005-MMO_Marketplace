933719c4708f449a79ba32fce622087ad7bdf6e1
demonlexe <alexiskaufman@gmail.com>
2024-11-01 09:49:46 -0500

    add margin to top

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div className="mt-5">{children}</div>
       </body>
     </html>
   );
