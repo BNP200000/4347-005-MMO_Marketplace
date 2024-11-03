ac1bb522f95552de278283faa9ef6417d5c6a044
demonlexe <alexiskaufman@gmail.com>
2024-11-03 10:28:44 -0600

    nitfix

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -6,7 +6,6 @@ export const config = {
     /*
      * Match all request paths except for the ones starting with:
      * - api (API routes)
-     * - login, register (login and register pages)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
