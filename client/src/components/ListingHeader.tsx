4d4e4733a330c0d64e4a362ce15ef9c3b11d2c4c
Chedd1 <qtm190000@utdallas.edu>
2024-11-10 11:46:54 -0600

    Connected individual listing page to backend
    Doesn't use the correct query yet though

--- client/src/components/ListingHeader.tsx
+++ client/src/components/ListingHeader.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import axios from "axios";
+import { Col, Container, Row, Form, Button } from "react-bootstrap";
+
+const PORT = process.env.PORT || 5001;
 
 // TODO: Rename this whole file
 
 export default function ListingHeader() {
   const params = useParams<{ listingId: string }>();
+  const [listingId, setListingId] = useState(params.listingId);
   const [characterName, setCharacterName] = useState("bingus");
+  const [category, setCategory] = useState("");
 
   // Axios.get or something similar using listingId
   // Communicates with index.ts
@@ -18,6 +23,45 @@ export default function ListingHeader() {
   // Then server side, login.ts has a post that handles the request and returns a response
   // login.ts uses queries pretty directly
 
+  /*useEffect(() => {
+  async function grabCrud (){*/
+  const importInfo = async (/*e: React.FormEvent*/) => {
+    // e.preventDefault();
+
+  try {
+    const res = await axios.post(`http://localhost:${PORT}/listing`, {
+      listingId
+    });
+    // Check for error message in response
+    if (res.data.error) {
+      // setError(res.data.error);
+      setCharacterName(res.data.error);
+      return res.data.error;
+    } else {
+      // Set login data in the cookie
+      // setLoginCookie({ username, password });
+      // Redirect to the home page
+      // router.push("/");
+      setCharacterName(res.data.listing.character_id);
+      return res.data.listing;
+    }
+  } catch {
+    // setError("An error occurred. Please try again.");
+    // setCategory("catch");
+  } finally {
+    // setLoading(false);
+    // setCharacterName(res.data.listing.character_id);
+    // setCharacterName("finally");
+  }
+
+};
+
+importInfo();
+
+/*}
+grabCrud();
+}, []);*/
+
   return(
     <Container>
       <Row><Col>
@@ -27,7 +71,7 @@ export default function ListingHeader() {
       <Row>
         <Col>
           <p>Name: {characterName}</p>
-          <p>Category</p>
+          <p>Category: {category}</p>
           <p>Rarity</p>
           <p>Price</p>
           <p>Allowed Classes</p>
